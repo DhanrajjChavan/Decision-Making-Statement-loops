7cf0e05c4e6ba600ab98d6c688a438cde035b64e
DhanrajjChavan <dhanarajchavan777@gmail.com>
2024-10-05 00:29:01 +0530

    Create 1)for loop.cpp

---
+++ 1)for loop.cpp
@@ -0,0 +1,20 @@
+/*Name: Dhanraj Chavan
+PRN: 24070123502
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        cout<<"SIT"<<endl;
+    }
+}
+
+/*
+SIT
+SIT
+SIT
+SIT
+SIT
+*/
